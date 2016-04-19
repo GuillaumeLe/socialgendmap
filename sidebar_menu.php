2e3cb54e784491115abba666ec8fac71f39fe0a7
Sheld0 <bouchutquentin@gmail.com>
2016-04-19 14:10:59 +0200

    ajout recherche

--- sidebar_menu.php
+++ sidebar_menu.php
@@ -6,6 +6,7 @@
         <ul class="sidebar-tabs" role="tablist">
             <li><a href="#social" role="tab"><i class="fa fa-users"></i></a></li>
             <li><a href="#search" role="tab"><i class="fa fa-search"></i></a></li>
+						<li><a href="#result" role="tab"><i class="fa fa-search"></i></a></li>
 	    </ul>
 
         <!-- Tab panes -->
@@ -16,9 +17,9 @@
                 <h3><input type="checkbox" name="twitter" value="twitter"><i class="fa fa-twitter"></i> Twitter<br></h3>
                 <h3><input type="checkbox" name="facebook" value="facebook"><i class="fa fa-facebook"></i> Facebook<br></h3>
                 <h3><input type="checkbox" name="instagram" value="instagram"><i class="fa fa-instagram"></i> Instagram<br></h3>
-                
+
                 </br></br>
-                
+
                 <p>Localisation :</br></p>
 				<p>Latitude :</br><INPUT type="text" name="locx" id="locx" autofocus>
 				</p>
@@ -38,7 +39,7 @@
 				<INPUT type="submit" value="RECHERCHER" >
 
 			</div>
-			
+
 			<!--tab RECHERCHE-->
 			<div class="sidebar-pane" id="search">
 				<h1>Recherche</h1>
@@ -53,13 +54,13 @@
 				<div id="res_com"></div>
 				</FORM>
 				Recherche floue IGN, base adresse nationale et nominatim
-				
+
 <!-- ===========================================================================================================================================================================================
 // SCRIPT dans l'onglet RECHERCHE qui affiche les résultats d'une recherche par catégorie (nominatim, BAN, bdnyme, IGN)
 //========================================================================================================================================================================================= -->
 
 				<script>
-				
+
 				//fonction requête rech
 				$( "#search" ).submit(function( event ) {
 					  // Stop form from submitting normally
@@ -77,7 +78,7 @@
 				posting.done(function( data ) {
 					var content = $( data );
 					$( "#res_BAN" ).empty().append( content );
-					});	
+					});
 				url = "./services/bdnyme.php";
 				// Send the data using post
 				var posting = $.get( url, { c: term2 , pt1_x: nw.lng, pt1_y: nw.lat, pt2_x: se.lng, pt2_y: se.lat}) ;
@@ -94,28 +95,16 @@
 					});
 
 				});
-				
-				
-							//handler click sur résultat BAN
-	$(document).on('click', '#ban-table tr', function() {
-    $(this).closest("tr").siblings().removeClass("highlighted");
-    $(this).toggleClass("highlighted");
-	var x = $(this).find('td').first().text();
-	var y = $(this).find('td').last().text();
-	var nom = $(this).find('.nom').text();
-    map.setView([y, x],16);
-	L.marker([y, x]).addTo(map).bindPopup("<STRONG>"+nom+"</STRONG></BR>DÂ°D LAT : "+y+" - LONG : "+x+"</BR>DÂ°M.M' LAT : "+ConvertDDToDMM(y)+" - LONG : "+ConvertDDToDMM(x)+"</BR>DÂ°M'S'' LAT : "+ConvertDDToDMS(y)+" - LONG : "+ConvertDDToDMS(x));
-	document.getElementById("locy").value=y;
-	document.getElementById("locx").value=x;
-	//document.location.replace("./gendloc.php#social");
-	});
 				</script>
-				
+
 <!-- ===========================================================================================================================================================================================
 // Reprise de l'affichage de la page en HTML
 //========================================================================================================================================================================================= -->
-				
-			</div>
 
-		</div>
+			</div>
+			<div class="sidebar-pane" id="result">
+				<h1>Resultats</h1>
+		  </div>
+
+</div>
 	</div>

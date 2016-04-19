0196d9e0c70b6296e3a12a716e87dbe346cd7530
Guillaume LEROY <gleroy@ensta.fr>
2016-04-19 08:25:35 +0200

    sidebar

--- carte.php
+++ carte.php
@@ -9,42 +9,42 @@
 
   // Cle API Geoportail #}
   var apiKey = "XXXXXXXXXXXXXXXXXXXXXXXX";
-  
+
   // Liste des couches a afficher #}
   var layers = new Array();
-  
+
   // L'url des services Geoportail #}
   function geopUrl (key, layer, format)
   {	return "http://wxs.ign.fr/"+ key + "/wmts?LAYER=" + layer
   +"&EXCEPTIONS=text/xml&FORMAT="+(format?format:"image/jpeg")+"&SERVICE=WMTS&VERSION=1.0.0&REQUEST=GetTile&STYLE=normal&TILEMATRIXSET=PM"
   +"&TILEMATRIX={z}&TILECOL={x}&TILEROW={y}" ;
   }
- 
+
   var attributionIGN = '&copy; <a href="http://www.ign.fr/">IGN-France</a>';
-		 
+
   // Definition des couches #}
   // Carte IGN #}
   var SCAN = L.tileLayer(geopUrl(apiKey,"GEOGRAPHICALGRIDSYSTEMS.MAPS"), { attribution:attributionIGN, maxZoom:18 } );
   var SCANEXPRESS = L.tileLayer(geopUrl(apiKey,"GEOGRAPHICALGRIDSYSTEMS.MAPS.SCAN-EXPRESS.CLASSIQUE"), { attribution:attributionIGN, maxZoom:18 } );
   var ORTHO = L.tileLayer(geopUrl(apiKey,"ORTHOIMAGERY.ORTHOPHOTOS"), { attribution:attributionIGN, maxZoom:19 } );
 
-  
-  
+
+
   // Carte Open OpenStreetMap_Mapnik #}
   var osmfr = L.tileLayer('http://{s}.tile.openstreetmap.fr/osmfr/{z}/{x}/{y}.png', {
   attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap France</a>'
   });
 
-  
+
 // centre la carte sur l'ENSTA
   var center_x = 2.2187066;
   var center_x = -105.02; // USA
   var center_y = 48.7110092;
-  var center_y = 39.5; //USA 
+  var center_y = 39.5; //USA
 
-  var center_z = 12; // définition du zoom
-  
-  // marker resultat geoloc		
+  var center_z = 12; // dï¿½finition du zoom
+
+  // marker resultat geoloc
   var locMarker = L.Icon.extend({
           options: {
               iconUrl: "{{ asset('images/icon/loc.png') }}", //POINT ROUGE
@@ -55,7 +55,7 @@
   // marker pulse icon
   var pulsingIcon = L.icon.pulse({iconSize:[20,20],color:'red'});
 
-  
+
   // La carte Leaflet #}
   var map = L.map("map", {
       center: new L.LatLng(center_y, center_x),
@@ -100,7 +100,7 @@ var delay=5000000; //5 seconds
 
 setTimeout(function() {
 map.removeLayer(cities);
-  
+
 }, delay);
 
 
@@ -108,15 +108,15 @@ map.removeLayer(cities);
   // ajout de la couche IGN SCAN #}
  //map.addLayer(SCAN);
  map.addLayer(osmfr);
- 
 
-  
+
+
   // rÃ©cupÃ©ration de la boundingbox #}
   nw = map.getBounds().getNorthWest();
   se = map.getBounds().getSouthEast();
 
- 
-  
+
+
   // initialisation et ajout menu couches #}
   var baseMaps = [{
       groupName : "OSM",
@@ -132,30 +132,30 @@ map.removeLayer(cities);
           "SÃ©ries EXPRESS": SCANEXPRESS,
           "SÃ©rie Orthophoto": ORTHO
       }
-  }];  
+  }];
 
   var overlays = [];
 
   var options = {
       container_width : "200px",
-      container_maxHeight : "400px", 
+      container_maxHeight : "400px",
       group_maxHeight : "150px",
       exclusive : false
   };
-		
+
   var control = L.Control.styledLayerControl(baseMaps, overlays, options);
   map.addControl(control);
 
- 
-  
+
+
   // affichage sidebar #}
   var sidebar = L.control.sidebar('sidebar').addTo(map);
 
-  
-  // affichage échelle en bas à gauche #}
+
+  // affichage ï¿½chelle en bas ï¿½ gauche #}
   L.control.scale({'position':'bottomleft','metric':true,'imperial':false }).addTo(map);
 
-  // affichage coordonnÃ©es curseur DD en bas à droite #}
+  // affichage coordonnÃ©es curseur DD en bas ï¿½ droite #}
   L.control.coordinates(
   {
       useLatLngOrder:true,
@@ -163,8 +163,8 @@ map.removeLayer(cities);
       labelTemplateLat:"D.D Lat {y}",
       labelTemplateLng:"Lng {x}"
   }).addTo(map);
-  
-  // affichage coordonnÃ©es curseur DMS en bas à droite #}
+
+  // affichage coordonnÃ©es curseur DMS en bas ï¿½ droite #}
   L.control.coordinates({
       position:"bottomright",
       useDMS:true,
@@ -179,7 +179,7 @@ map.removeLayer(cities);
       nw=map.getBounds().getNorthWest();
       se=map.getBounds().getSouthEast();
   });
-  
+
   map.on('zoomend ', function(e)
   {
       if (map.getZoom() >= 15)
@@ -195,25 +195,25 @@ map.removeLayer(cities);
 
 
 
-// fonction permettant, au moment où l'on clique sur une ligne du tableau contenant les résultats de recherche pour NOMINATIM, de faire apparaitre le marker en y incluant un popup qui apparait lorsqu'on clique sur le marker
+// fonction permettant, au moment oï¿½ l'on clique sur une ligne du tableau contenant les rï¿½sultats de recherche pour NOMINATIM, de faire apparaitre le marker en y incluant un popup qui apparait lorsqu'on clique sur le marker
 	$(document).on('click', '#nominatim-table tr', function() {
       $(this).closest("tr").siblings().removeClass("highlighted");
       $(this).toggleClass("highlighted");
-      // récupération des données présentes dans les class de searchResult.html.twig
+      // rï¿½cupï¿½ration des donnï¿½es prï¿½sentes dans les class de searchResult.html.twig
 	    var y = $(this).find('.lat').text();
 	    var x = $(this).find('.lng').text();
 	    var nom = $(this).find('.nom').text();
       map.setView([y, x],16);
 	    L.marker([y, x]).addTo(map).bindPopup("<STRONG>"+nom+"</STRONG></BR>DÂ°D LAT : "+y+" - LONG : "+x+"</BR>DÂ°M.M' LAT : "+ConvertDDToDMM(y)+" - LONG : "+ConvertDDToDMM(x)+"</BR>DÂ°M'S'' LAT : "+ConvertDDToDMS(y)+" - LONG : "+ConvertDDToDMS(x));
-	});	
+	});
 
 
 
-// fonction permettant, au moment où l'on clique sur une ligne du tableau contenant les résultats de géoloc, de faire apparaitre le marker de géoloc en y incluant un popup qui apparait lorsqu'on clique sur le marker
+// fonction permettant, au moment oï¿½ l'on clique sur une ligne du tableau contenant les rï¿½sultats de gï¿½oloc, de faire apparaitre le marker de gï¿½oloc en y incluant un popup qui apparait lorsqu'on clique sur le marker
 $(document).on('click', '#res_loc tr', function() {
     $(this).closest("tr").siblings().removeClass("highlighted");
     $(this).toggleClass("highlighted");
-    // récupération des données présentes dans les class de smsloc.html.twig
+    // rï¿½cupï¿½ration des donnï¿½es prï¿½sentes dans les class de smsloc.html.twig
     var x = $(this).find('.lng').text();
 	var y = $(this).find('.lat').text();
 	var tel = $(this).find('.tel').text();
--- sidebar_menu.php
+++ sidebar_menu.php
@@ -1,12 +1,30 @@
-<!-- Définition de la sidebar -->
-
-  <ul class="sidebar-tabs" role="tablist" style="z-index:1;">
-    <li><a href="#couches" role="tab"><i class="fa fa-home"></i></a></li>
-    <li><a href="#search" role="tab"><i class="fa fa-search"></i></a></li>
-    <li><a href="#geoloc" role="tab"><i class="fa fa-map-marker"></i></a></li>
-    <li><a href="#messages" role="tab"><i class="fa fa-envelope"></i></a></li>
-    <li><a href="#admin" role="tab"><i class="fa fa-cog"></i></a></li>
-    <li><a href="#logout" role="tab"><i class="fa fa-power-off"></i></a></li>
-  </ul>
-
-     
+<!-- Définition de la sidebar -->
+
+<!-- SIDEBAR -->
+	<div id="sidebar" class="sidebar collapsed">
+        <!-- Nav tabs -->
+        <ul class="sidebar-tabs" role="tablist">
+            <li><a href="#home" role="tab"><i class="fa fa-home"></i></a></li>
+	    </ul>
+
+        <!-- Tab panes -->
+        <div class="sidebar-content active">
+            <!--tab HOME-->
+			<div class="sidebar-pane" id="home">
+                <h1>Réseaux sociaux</h1>
+                <p>Localisation</p>
+				<p>x :<INPUT type="TEXT" name="locx" id="locx" autofocus>
+				</p>
+				<p>y :<INPUT type="TEXT" name="locy" id="locy" autofocus>
+				</p>
+				<p>Rayon de recherche
+				<INPUT type="TEXT" name="r" id="r" autofocus>
+				</p>
+                <p>Date
+				<INPUT type="TEXT" name="date" id="date" autofocus>
+				</p>
+				<INPUT type="submit" value="RECHERCHER" >
+
+			</div>
+		</div>
+	</div>
